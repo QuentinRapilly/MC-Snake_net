2beda0f1fcce2d28d8ceae676fa37c740c81fc17
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2023-02-22 17:24:45 +0100

    [*]Singularity working

--- src/loss_functions/consistency_tools.py
+++ src/loss_functions/consistency_tools.py
@@ -3,11 +3,10 @@
 from shapely.geometry import Polygon, Point
 import torch
 from torch.nn.functional import pad
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from cv2 import findContours, RETR_EXTERNAL, CHAIN_APPROX_NONE
 import numpy as np
 
-# Changer la seconde fonction pour utiliser cv2.findContours(img, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
 # Changer les deux fonctions pour passer en multiobjet
 
 def contour_to_mask(contour_samples : torch.tensor, W : int, H : int):
@@ -23,6 +22,7 @@ def contour_to_mask(contour_samples : torch.tensor, W : int, H : int):
 
 
 def mask_to_contour(mask : torch.tensor, only_one = True , add_last = False, verbose = False):
+    
     contours, _ = findContours(mask.numpy().astype(np.uint8), mode=RETR_EXTERNAL, method=CHAIN_APPROX_NONE)
 
     if len(contours)==0:
@@ -34,81 +34,6 @@ def mask_to_contour(mask : torch.tensor, only_one = True , add_last = False, ver
     
     return contours
 
-def former_mask_to_contour(mask : torch.tensor, add_last = False, verbose = False):
-
-    plt.imshow(mask)
-    plt.show()
-
-    W, H = mask.shape
-
-    mask = pad(mask, (1,1,1,1), mode="constant", value=0)
-
-    moore_pixel_to_ind = [[0,1,2],[7,-1,3],[6,5,4]]
-    moore_ind_to_pixel = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
-
-    ### Moore Neighborhood ###
-    # 
-    #    # 0, 1, 2 #
-    #    # 7, x, 3 #
-    #    # 6, 5, 4 #
-    #
-    ##########################
-    
-    # find the first non black pixel
-    i,j = 1,0
-    greater_than_one = False
-
-    while greater_than_one == False:
-        j += 1
-        while i<W and mask[i,j]==0:
-            j = j+1
-            if j >= H:
-                i = i+1
-                j = 0
-
-        if i==W :
-            return torch.tensor([[]])
-
-        current = [i,j]
-
-        if verbose : print("First pixel : {}".format(current))
-
-        contour = [current]
-        
-        moore_ind = 0 # we came in the first contour pixel by increasing j (pos 7 in Moore neighborhood, so the following one is 0)
-
-        while moore_ind < 8 and mask[i+moore_ind_to_pixel[moore_ind][0],j+moore_ind_to_pixel[moore_ind][1]]==0:
-            moore_ind +=1
-
-        if moore_ind<8:
-            greater_than_one = True
-
-    following = [i+moore_ind_to_pixel[moore_ind][0],j+moore_ind_to_pixel[moore_ind][1]]
-
-
-    while following != contour[0]:
-        contour.append(following)
-
-        previous = current
-        current = following 
-
-        i, j = current[0], current[1]
-        di, dj = previous[0] - i, previous[1] - j 
-
-
-        moore_ind = (moore_pixel_to_ind[di+1][dj+1] + 1) % 8
-
-
-        while mask[i+moore_ind_to_pixel[moore_ind][0],j+moore_ind_to_pixel[moore_ind][1]]==0:
-            moore_ind = (moore_ind + 1) % 8
-
-        following = [i+moore_ind_to_pixel[moore_ind][0],j+moore_ind_to_pixel[moore_ind][1]]
-
-    if add_last : contour.append(following)
-
-    if verbose : print("Contour : {}".format(contour))
-
-    return torch.tensor(contour) - 1
 
 
 if __name__ == "__main__":
@@ -122,9 +47,9 @@ if __name__ == "__main__":
     mask = contour_to_mask(contour, W, H)
 
 
-    plt.imshow(mask.T)
+    #plt.imshow(mask.T)
     #plt.plot(contour[0,:],contour[1,:])
 
-    plt.show()
+    #plt.show()
 
 
