f1adca13f8bef56c975c36477a2bb04442f7a440
Quentin Rapilly <qrapilly@igrida-frontend.irisa.fr>
2023-02-23 17:07:13 +0100

    [*] Modifs on igrida scripts

--- igrida.sh
+++ igrida.sh
@@ -4,4 +4,4 @@ oarsub \
     -O igrida.o \
     -E igrida.e \
     -l /gpu_device=1/core=4,walltime=01:00:00 \
-    bash run.sh
+    "bash run.sh"
--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd /nfs/nas4/serpico_1/serpico/qrapilly
+cd /nfs/nas4/serpico_1/serpico/qrapilly/MC-Snake_net
 
+. /etc/profile.d/modules.sh
 module load spack/singularity
-singularity --nv -B /srv:/srv MC-net_image.sif
-
-cd MC-Snake_net
-python src/train.py
+singularity exec -B /srv:/srv --nv ../MC-net_image.sif python src/train.py
