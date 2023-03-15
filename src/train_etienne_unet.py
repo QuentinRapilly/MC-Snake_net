5d7e1a2211dc99cab6ae011055b2a48a5f5b36ab
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2023-03-15 16:45:09 +0100

    [+] Train file with general unet

---
+++ src/train_etienne_unet.py
@@ -0,0 +1,212 @@
+import torch
+from torch.utils.data import DataLoader
+import argparse
+import json
+from time import time
+import wandb
+from torch.nn import MSELoss, BCEWithLogitsLoss, CrossEntropyLoss
+from torch import sigmoid
+from datetime import datetime
+from os.path import join, isdir
+from os import mkdir
+from torch.optim.lr_scheduler import ExponentialLR
+
+import matplotlib.pyplot as plt # enlever quand le probleme est regle
+
+from datasets.texture_dataset import TextureDataset
+
+from DL_models.u2D_2D import Unet2D_2D
+from DL_models.mcsnakenet_clean import MCSnakeNet
+
+
+from loss_functions.consistency_loss import MutualConsistency, DiceLoss, SnakeLoss
+
+
+def train(model, optimizer, train_loader, mask_loss, epoch : int, apply_sigmoid : bool, verbose = False):
+
+    tic_epoch = time()
+
+    running_loss = 0.0
+
+    N = len(train_loader)
+
+
+    ### To remove after debugging ###
+    # From HERE
+    plot_dir = "/net/serpico-fs2/qrapilly/model_storage/MC-snake/epoch_{}".format(epoch)
+    if not isdir(plot_dir):
+        mkdir(plot_dir)
+    # To HERE
+
+    for k, batch in enumerate(train_loader):
+
+        print("Progress of the epoch : {}%          \r".format(round(k/len(train_loader)*100,ndigits=2)), end="")
+
+        B = train_loader.batch_size
+        imgs, GT_masks = batch
+
+        optimizer.zero_grad()
+        
+        # Model applied to input
+        tic_forward = time()
+        classic_mask, control_points = model(imgs)
+        tac_forward = time()
+
+        if apply_sigmoid :
+            classic_mask = sigmoid(classic_mask)
+            
+        classic_mask = torch.squeeze(classic_mask)
+        
+
+        if B==1:
+            classic_mask = torch.unsqueeze(classic_mask,0)
+        loss = mask_loss(classic_mask, GT_masks)
+
+
+        # Backward gradient step
+        tic_backward = time()
+        loss.backward()
+        optimizer.step()
+        tac_backward = time()
+
+        running_loss += loss.item()
+
+        if k<N-1:
+            plt.figure(figsize = (20,10))
+            for i in range(B):
+                plt.subplot(3,B,1+i)
+                plt.imshow(torch.squeeze(imgs[i]).detach().cpu(), cmap="gray", vmin=0, vmax=1)
+
+            for i in range(B):
+                plt.subplot(3,B,1+B+i)
+                plt.imshow(GT_masks[i].detach().cpu(), cmap="gray", vmin=0, vmax=1)
+            
+            for i in range(B):
+                plt.subplot(3,B,1+2*B+i)
+                plt.imshow(sigmoid(classic_mask[i]).detach().cpu(), cmap="gray", vmin=0, vmax=1)
+                #plt.imshow(classic_mask[i].detach().cpu(), cmap="gray", vmin=0, vmax=1)
+
+            plt.savefig(join(plot_dir,"batch_{}.png".format(k)))
+
+            plt.close()
+
+        # Choosing some image to plot in the WAndB recap
+        if k == len(train_loader) - 1:
+            plot_res = (GT_masks[0], sigmoid(classic_mask[0])) #, snake_mask[0]) if print_in_table else None
+    
+
+    tac_epoch = time()
+
+    print("Epoch terminated in {}s".format(tac_epoch-tic_epoch))
+
+    return running_loss / N, plot_res
+
+
+
+if __name__ == "__main__" :
+
+    # TODO : creer une fonction de test
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    print(f"Current device : {device}")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", type=str, help="Configuration file", default="./src/config/basic_config.json")
+    args = parser.parse_args()
+
+    config = args.config
+
+    # Loading the config dic
+    with open(config, "r") as f:
+        config_dic = json.load(f)
+
+    # Getting all the config information required into the config dic
+
+    train_config = config_dic["data"]["train_set"]
+    test_config = config_dic["data"]["test_set"]
+    settings_config = config_dic["settings"]
+
+    verbose = settings_config["verbose"]
+
+    model_config = config_dic["model"]
+    optimizer_config = config_dic["optimizer"]
+    criterion_config = config_dic["criterion"]
+    snake_config = config_dic["active_contour"]
+
+    W, H = config_dic["data"]["image_size"]
+
+    train_set_index = train_config["set_index"]
+    test_set_index = test_config["set_index"]
+
+    train_set = TextureDataset(path=train_config["path_to_data"], subset=train_set_index, device = device)
+    test_set = TextureDataset(path=train_config["path_to_data"], subset=test_set_index, device = device)
+
+    train_loader = DataLoader(train_set, batch_size=train_config["batchsize"])
+    test_loader = DataLoader(test_set, batch_size=test_config["batchsize"])
+
+
+
+    # Initializing the model
+    #model = Unet2D_2D(num_classes =model_config["num_class"], input_channels=config_dic["data"]["nb_channels"],\
+    #                  padding_mode="zeros", train_bn=False, inner_normalisation='BatchNorm').to(device)
+    model = MCSnakeNet(num_classes =model_config["num_class"], input_channels=config_dic["data"]["nb_channels"],\
+                       padding_mode="zeros", train_bn=False, inner_normalisation='BatchNorm', img_shape=(W,H),\
+                        nb_control_points=model_config["nb_control_points"]).to(device)
+
+
+    # Initializing the optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=optimizer_config["lr"])
+
+    scheduler_config = config_dic["scheduler"]
+    scheduler_step = scheduler_config["step_every_nb_epoch"]
+    scheduler = ExponentialLR(optimizer=optimizer, gamma=scheduler_config["gamma"])
+
+    loss_config = config_dic["loss"]
+
+    # Initializing the loss 
+    mask_loss = {"bce": BCEWithLogitsLoss(), "dice": DiceLoss(), "mse" : MSELoss() }[loss_config["loss_name"]]
+    apply_sigmoid = loss_config["apply_sigmoid"]
+
+
+
+    # Tracking of the loss and some images during the training
+    run = wandb.init(
+        # set the wandb project where this run will be logged
+        project="MC-snake_net",
+        
+        # track hyperparameters and run metadata
+        config={
+        "learning_rate": optimizer_config["lr"],
+        "architecture": "UNET",
+        "dataset": "Texture",
+        "epochs": train_config["nb_epochs"]
+        }
+    )
+
+    epoch_modulo = train_config["print_every_nb_epochs"]
+
+    for epoch in range(train_config["nb_epochs"]):
+
+        print(f"Starting epoch {epoch}")
+        loss, plot_res = \
+                train(model, optimizer, train_loader, mask_loss=mask_loss, apply_sigmoid=apply_sigmoid,\
+                    epoch=epoch, verbose=verbose)
+
+        
+        gt, proba = plot_res #, snake
+        gt = wandb.Image(gt, caption="GT")
+        proba = wandb.Image(proba, caption="Probability map")
+        #snake = wandb.Image(snake, caption="Snake mask")
+
+        wandb.log({"loss": loss, "GT" : gt,\
+                        "Probability map" : proba})
+        
+        if (epoch + 1)%scheduler_step == 0:
+            scheduler.step()
+        
+
+    wandb.finish()
+
+    now = datetime.now()
+    torch.save(model.state_dict(), join(model_config["save_path"],str(now).replace(" ","_").split(".")[0]+".pkl"))
