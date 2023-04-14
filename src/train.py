389dbca50dee5d4fef9b3e9a520e48fc52f8686e
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2023-04-14 14:57:58 +0200

    [*]Correction of polar coordinates radius

--- src/train.py
+++ src/train.py
@@ -71,7 +71,7 @@ def train(model, unet_optimizer, mlp_optimizer, train_loader, mask_loss, snake_l
         reshaped_cp = torch.reshape(snake_cp, (snake_cp.shape[0], snake_cp.shape[1]//2, 2))
 
         if use_polar :
-            reshaped_cp = polar_to_cartesian_cp(c = reshaped_cp[...,0], r = reshaped_cp[...,1:,0], theta = reshaped_cp[...,1:,1])
+            reshaped_cp = polar_to_cartesian_cp(c = reshaped_cp[...,0], r = 0.5*reshaped_cp[...,1:,0], theta = reshaped_cp[...,1:,1])
 
         if predict_dx_dy :
             M = reshaped_cp.shape[1]
@@ -181,7 +181,7 @@ def test(model, test_loader, mask_loss, snake_loss, theta, gamma, W : int, H : i
         reshaped_cp = torch.reshape(snake_cp, (snake_cp.shape[0], snake_cp.shape[1]//2, 2))
 
         if use_polar :
-            reshaped_cp = polar_to_cartesian_cp(c = reshaped_cp[...,0], r = reshaped_cp[...,1:,0], theta = reshaped_cp[...,1:,1])
+            reshaped_cp = polar_to_cartesian_cp(c = reshaped_cp[...,0], r = 0.5*reshaped_cp[...,1:,0], theta = reshaped_cp[...,1:,1])
 
         if predict_dx_dy :
             M = reshaped_cp.shape[1]
