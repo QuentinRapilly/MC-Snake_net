48cad11e37bd1a9f2c42fbd156adf927bf6fa5d4
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2023-06-14 17:18:38 +0200

    [*]Train BN

--- src/train.py
+++ src/train.py
@@ -289,7 +289,7 @@ if __name__ == "__main__" :
     model = MCSnakeNet(num_classes =model_config["num_class"], input_channels=config_dic["data"]["nb_channels"],\
                        padding_mode="zeros", inner_normalisation='BatchNorm', img_shape=(W,H),\
                         nb_control_points=M_prime, nb_snake_layers=model_config["nb_snake_layers"],\
-                            train_bn = True, inner_normalisation='BatchNorm').to(device)
+                            train_bn = True).to(device)
 
 
     # Initializing the optimizer
